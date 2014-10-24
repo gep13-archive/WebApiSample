ff2e3523a9283f5403d634236ff203e0665c27ef
Gary Ewan Park <gep13@gep13.co.uk>
2014-10-24 19:44:59 +0100

    Add SQL File to seed single chemical

---
+++ Source/Gep13.Sample.Database/Scripts/Script0003 - Seed Chemical Data.sql
@@ -0,0 +1,9 @@
+﻿/****** Object: Table [dbo].[Chemicals] Script Date: 24/10/2014 19:39:45 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+INSERT INTO dbo.Chemicals(Name, IsArchived, Balance)
+VALUES ('Poison','False','13');
